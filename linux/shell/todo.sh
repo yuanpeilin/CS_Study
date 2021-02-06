b0125841cb33c170d9771c35985df7b9069b5316
Ubuntu18 <telaluosifen@gmail.com>
2021-02-06 10:52:28 +0800

    [TODO] change path to /var/todo/todo

--- linux/shell/todo.sh
+++ linux/shell/todo.sh
@@ -1,10 +1,12 @@
+path=/var/todo/todo
+
 todo_help(){
     echo -e "\e[1m Usage: \e[0m"
     echo -e "    -a <string>   add a task"
     echo -e "    -c            clear all finished tasks"
     echo -e "    -d <number>   done"
     echo -e "    -h            help"
-    echo -e "    -i            edit"
+    echo -e "    -i            edit data file: /var/todo/todo"
     echo -e "    -l            list all tasks"
     echo -e "    -L            list all tasks with time"
     echo -e "    -r <number>   remove the finished task"
@@ -25,7 +27,7 @@ todo_list_task(){
             printf "\e[09m%-2s %s %s \n\e[00m" "$i [*]" "${content:0:30}" "   $date"
         fi
         i=$((i+1))
-    done < ~/.todo
+    done < "$path"
     unset i line status content date
 }
 
@@ -34,17 +36,17 @@ todo_getopts(){
         case "$arg" in
             a) # Add
                 date=$(date +'%Y/%m/%d')
-                echo "U $date $OPTARG" >> ~/.todo
+                echo "U $date $OPTARG" >> "$path"
                 unset date
                 todo_list_task
                 ;;
             c) # Clean
-                line_list=$(grep -nE --text '^D' ~/.todo | cut -d ":" -f 1)
+                line_list=$(grep -nE --text '^D' "$path" | cut -d ":" -f 1)
                 i=0
                 for var in $line_list
                 do
                     var=$((var-i))
-                    sed -i "$var"d ~/.todo
+                    sed -i "$var"d "$path"
                     i=$((i+1))
                 done
                 unset var i line_list
@@ -52,28 +54,28 @@ todo_getopts(){
                 ;;
             d) # Done
                 temp="$OPTARG"'s/U/D/'
-                sed -i "$temp" ~/.todo
+                sed -i "$temp" "$path"
                 unset temp
                 todo_list_task
                 ;;
             i) # edit
-                vim ~/.todo;;
+                vim "$path";;
             l) # List
                 todo_list_task;;
             L) # List with time
                 todo_list_task "time";;
             r) # Remove
-                if_done=$(sed -n "$OPTARG"p ~/.todo | cut -d " " -f 1)
+                if_done=$(sed -n "$OPTARG"p "$path" | cut -d " " -f 1)
                 if [[ "$if_done" == U ]]; then
                     echo -e "\e[1;31m WARNING: \e[0m task is not finished! Use -R instead."
                 else
-                    sed -i "$OPTARG"d ~/.todo
+                    sed -i "$OPTARG"d "$path"
                     todo_list_task
                 fi
                 unset if_done
                 ;;
             R) # force Remove
-                sed -i "$OPTARG"d ~/.todo
+                sed -i "$OPTARG"d "$path"
                 todo_list_task
                 ;;
             *)
@@ -92,3 +94,4 @@ main(){
 }
 
 main "$@"
+unset path
